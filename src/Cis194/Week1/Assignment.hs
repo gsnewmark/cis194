b656db3f1a19ac7816a748f0e554bfdbfddc5fd8
Ivan Kryvoruchko <gildraug@gmail.com>
2015-01-17 18:44:43 +0200

    Add week 1 HW

--- src/Cis194/Week1/Assignment.hs
+++ src/Cis194/Week1/Assignment.hs
@@ -1,1 +1,35 @@
 module Cis194.Week1.Assignment where
+
+toDigits :: Integer -> [Integer]
+-- ^Convert positive Integers to a list of digits
+toDigits n = reverse (toDigitsRev n)
+
+toDigitsRev :: Integer -> [Integer]
+-- ^Convert positive Integers to a list of digits with digits reversed
+toDigitsRev n
+  | n <= 0    = []
+  | otherwise = mod n 10 : toDigitsRev (div n 10)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:y:xs)
+  | mod (length xs) 2 == 1 = x : (y * 2) : doubleEveryOther xs
+  | otherwise              = (x * 2) : y : doubleEveryOther xs
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs)
+  | div x 10 /= 0 = div x 10 + mod x 10 + sumDigits xs
+  | otherwise     = x + sumDigits xs
+
+validate :: Integer -> Bool
+validate n = mod (sumDigits (doubleEveryOther (toDigits n))) 10 == 0
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n s d t
+  | n <= 0    = []
+  | n == 1    = [(s, d)]
+  | otherwise = hanoi (n - 1) s t d ++ [(s, d)] ++ hanoi (n - 1) t d s
